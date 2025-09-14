8cb3d6253afae0f7b8744be668dfee8f3b9014a3
carlsj <carlsj@hanmail.net>
2025-09-14 19:17:50 +0900

    account신규

---
+++ volume_pro/src/volume_pro/Account.java
@@ -0,0 +1,30 @@
+package volume_pro;
+
+import java.math.BigDecimal;
+
+public class Account {
+
+	private String accountNumber;
+	private String accountBook;
+	private BigDecimal trxAmt;
+	
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getAccountBook() {
+		return accountBook;
+	}
+	public void setAccountBook(String accountBook) {
+		this.accountBook = accountBook;
+	}
+	public BigDecimal getTrxAmt() {
+		return trxAmt;
+	}
+	public void setTrxAmt(BigDecimal trxAmt) {
+		this.trxAmt = trxAmt;
+	}
+
+}
